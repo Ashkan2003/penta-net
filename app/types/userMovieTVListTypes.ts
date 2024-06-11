dd2ca41dc34ae14767f5d6e2ea386d3014c8446d
ashkan <ashkanpc2003@gmail.com>
2024-06-11 19:24:48 +0330

    fix the type issues of userListPage

--- app/types/userMovieTVListTypes.ts
+++ app/types/userMovieTVListTypes.ts
@@ -1,4 +1,12 @@
+import { movieListType } from "./movieTypes";
+import { tvSeriesListType } from "./tvSeriesTypes";
+
 export type miniUserMovieTVListType = {
   mediaTmdbId: number;
   mediaType: "movie" | "tvSeries";
 };
+
+// in this type we extends from movieListType, tvSeriesListType and we add a type named "mediaType: string;"
+export interface userStoredListType extends movieListType, tvSeriesListType {
+  mediaType: string;
+}
--- app/userList/page.tsx
+++ app/userList/page.tsx
@@ -1,28 +1,25 @@
 "use client";
 import { useEffect, useState } from "react";
 import MovieCard from "../components/reusable-components/MovieCard";
-import { useMovieDetails } from "../react-query/movie/useMovieDetails";
 import { useUserMovieTVList } from "../react-query/userMovieTVList/useUserMovieTVList";
 import { getMovieDetails } from "../services/movie/getMovieDetails";
 import { getTVSeriesDetails } from "../services/tvSeries/getTVSeriesDetails";
-
-import { movieDetailsType, movieListType } from "../types/movieTypes";
-import { miniUserMovieTVListType } from "../types/userMovieTVListTypes";
-import { tvSeriesListType } from "../types/tvSeriesTypes";
 import FullPageLoadingSpinner from "../components/reusable-components/FullPageLoadingSpinner";
 import TVSeriesCard from "../components/reusable-components/TVSeriesCard";
+import { userStoredListType } from "../types/userMovieTVListTypes";
 
 export default function UserListPage() {
-  const [userStoredList, setUserStoredList] = useState<
-    movieListType[] | tvSeriesListType[]
-  >();
+  // the userStoredList have both the movie and tvSeries -details. so
+  const [userStoredList, setUserStoredList] = useState<userStoredListType[]>();
+
+  // get userMovieTVList from react query
   const { isLoadingUserMovieTVList, userMovieTVList } = useUserMovieTVList();
 
   useEffect(() => {
     if (isLoadingUserMovieTVList) return;
 
     const handleData = async () => {
-      const list: movieListType[] | tvSeriesListType[] = await Promise.all(
+      const list: userStoredListType[] = await Promise.all(
         userMovieTVList!.map(async (media) => {
           if (media.mediaType === "movie") {
             return getMovieDetails(media.mediaTmdbId).then((res) => ({
@@ -41,40 +38,23 @@ export default function UserListPage() {
     };
     handleData();
   }, [isLoadingUserMovieTVList, userMovieTVList]);
+
   console.log(userStoredList);
 
   if (!userStoredList) {
     return <FullPageLoadingSpinner />;
   }
+
   return (
     <div className="py-9 px-3 ">
       <p className="text-white text-xl pb-5">ذخیره شده ها</p>
       <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6 justify-items-center   gap-y-5 gap-x-3 ">
-        {userStoredList!.map(
-          (media, index: number) =>
-            media.mediaType === "movie" ? (
-              <MovieCard key={index} movie={media} />
-            ) : (
-              <TVSeriesCard key={index} tvSeries={media} />
-            )
-          //    {
-          //   media.mediaType === "movie" && (
-          //     <MovieCard key={index} movie={media} />
-          //   );
-          // }
-          // media.mediaType === "movie" && (
-          //   <MovieCard key={index} movie={media} />
-          // )
-          // media.mediaType === "movie" &&<MovieCard key={index} movie={media} />
-
-          // if(media.backdrop_path) return <div></div>
-          // media.mediaType === "movie" && (
-          //   <MovieCard key={index} movie={media} />
-          // )
-
-          // media.mediaType ==="movie" && (
-          // <TVSeriesCard key={index} tvSeries={media} />
-          // )
+        {userStoredList.map((media, index: number) =>
+          media.mediaType === "movie" ? (
+            <MovieCard key={index} movie={media} />
+          ) : (
+            <TVSeriesCard key={index} tvSeries={media} />
+          )
         )}
       </div>
     </div>
